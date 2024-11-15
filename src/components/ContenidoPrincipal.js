378d43c2de31aa49c40fa51c4795c9810b953ed9
Danilo Toro <danilotoro227@gmail.com>
2024-11-15 11:36:03 -0500

    modificaciones

--- src/components/ContenidoPrincipal.js
+++ src/components/ContenidoPrincipal.js
@@ -19,6 +19,8 @@ function ContenidoPrincipal() {
         style={{ maxWidth: "600px" }}>
         <h5 class="text-primary">Presentación</h5>
       </div>
+      <h1>Pedro Danilo Toro</h1>
+      <h1>Yasson Brayan Perenguez</h1>
       <Presentacion></Presentacion>
       <div
         class="text-center mx-auto wow fadeIn"
